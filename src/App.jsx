4b990110488e61327733f1a0f72cb542c31f32f1
timataliatov <ttaliatov@gmail.com>
2024-08-19 15:02:05 +0200

    StarRating components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Accordion from './components/accordion/index.jsx';
 import RandomColors from './components/randomColors/index.jsx';
+import StarRating from './components/starRating/index.jsx';
 
 function App() {
   return (
     <div className="">
+      {/* StarRating Component */}
+      {<StarRating />}
+
       {/* Accordion Component */}
-      {/* <Accordion /> */}
+      {<Accordion />}
 
       {/* RandomColors Component */}
-      <RandomColors />
+      {<RandomColors />}
+
+      {/* ... Component */}
+      {/* <... /> */}
+
+      {/* ... Component */}
+      {/* <... /> */}
     </div>
   );
 }
--- src/components/randomColors/index.jsx
+++ src/components/randomColors/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './styles.css';
 
 const RandomColors = () => {
   const [hex, setHex] = useState('#ffffff');
